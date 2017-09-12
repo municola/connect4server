6c8352bfa78e83c4267da1b43253efd8de7ac3bb
Nicolas Muntwyler <nicolas.muntwyler@gmail.com>
2017-09-12 17:10:40 +0200

    bug fixes! -> enemy name, disconnect

--- server.js
+++ server.js
@@ -16,7 +16,6 @@ function howManyPeople(arr) {
 }
 
 io.on('connection', (socket) => {
-  console.log(socket.id, 'connected');
   socket.on('connectMe', (username) => {
     members.push([socket.id, username]);
     socket.emit('connected');
@@ -35,17 +34,19 @@ io.on('connection', (socket) => {
     socket.emit('newGameMessage', 0, username, message);
     socket.to(roomId).emit('newGameMessage', 1, username, message);
   });
-  socket.on('subscribe', (roomId) => {
+  socket.on('subscribe', (roomId, username) => {
     const people = howManyPeople(howMany);
     if (people[roomId] === 1) {
       socket.join(roomId);
       howMany[roomId].push(socket.id);
+
+      socket.to(roomId).emit('playerJoined', 2, username);
+
       socket.emit('confirmed', roomId);
       socket.emit('ready', 'X', 'Your Turn');
       socket.to(roomId).emit('ready', 'O', 'Enemy Turn');
 
-      socket.emit('players', howMany[roomId][0]);
-      socket.to(roomId).emit('players', howMany[roomId][1]);
+      socket.to(roomId).emit('enemyName', username);
 
       socket.emit('firstTurn');
       socket.emit('update', howManyPeople(howMany));
@@ -54,10 +55,14 @@ io.on('connection', (socket) => {
     if (people[roomId] === 0) {
       socket.join(roomId);
       howMany[roomId].push(socket.id);
+
       socket.emit('confirmed', roomId);
       socket.emit('update', howManyPeople(howMany));
       socket.broadcast.emit('update', howManyPeople(howMany));
     }
+  });
+  socket.on('enemyName', (roomId, name) => {
+    socket.to(roomId).emit('enemyName', name);
   });
   socket.on('turn', (table) => {
     socket.to(socket.rooms[Object.keys(socket.rooms)[0]]).emit('turn', table);
@@ -65,34 +70,40 @@ io.on('connection', (socket) => {
   socket.on('winner', () => {
     socket.to(socket.rooms[Object.keys(socket.rooms)[0]]).emit('winner');
   });
-  socket.on('unsubscribe', (roomId) => {
+  socket.on('unsubscribe', (roomId, username) => {
     socket.leave(roomId);
     for (let i = 0; i < howMany.length; i++) {
       howMany[i] = howMany[i].filter((item) => {
         return item !== socket.id;
       });
     }
+    socket.to(roomId).emit('playerLeft', 2, username);
     socket.emit('unsubscribed');
     socket.emit('update', howManyPeople(howMany));
     socket.broadcast.emit('update', howManyPeople(howMany));
   });
   socket.on('disconnect', () => {
-    console.log(socket.id, 'disconnect');
-    for (let i = 0; i < howMany.length; i++) {
-      howMany[i] = howMany[i].filter((item) => {
-        return item !== socket.id;
-      });
-      for (let j = 0; j < 2; j++) {
-        if (howMany[i][j] === socket.id) {
-          socket.leave(howMany[i][j]);
-        }
+    let username = undefined;
+    for (let i = 0; i < members.length; i++) {
+      if (members[i][0] === socket.id) {
+        username = members[i][1];
       }
     }
-    console.log(members);
+    for (let i = 0; i < howMany.length; i++) {
+      for (let j = 0; j < 2; j++) {
+        if (howMany[i][j] === socket.id) {
+          socket.leave(i);
+          socket.to(i).emit('playerLeft', 2, username);
+        }
+        howMany[i] = howMany[i].filter((item) => {
+          return item !== socket.id;
+        });
+      }
+    }
     members = members.filter((item) => {
       return item[0] !== socket.id;
     });
-    console.log(members);
+    socket.broadcast.emit('userLeft', 2, username);
     socket.broadcast.emit('update', howManyPeople(howMany));
     socket.broadcast.emit('updateMembers', members);
   });
